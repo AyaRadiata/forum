92035117ea24360c999832ef9f572c115fcf6582
AyaAya-coder <aidynbulatov@gmail.com>
2025-03-24 23:18:31 +0500

    12312
    123

--- script.js
+++ script.js
@@ -14,54 +14,57 @@ const timeTable = document.getElementById("time")
 neededDateDay = 19
 neededDateMonth = 4
 
-setInterval(()=>{
-    date = new Date()
-
-    nowMonth = date.getMonth() + 1
-    nowDay = date.getDate()
-    nowHours = date.getHours()
-    nowMinutes = date.getMinutes()
-    nowSeconds = date.getSeconds()
+if(timeTable){
+    setInterval(()=>{
     
-    if(nowMonth == 3){
-        nowDay = 31 + neededDateDay - 1 - nowDay
-    }
-    if(nowMonth == 2){
-        nowDay = 31 + 28 + neededDateDay - 1 - nowDay
-    }
-    if(nowMonth == neededDateMonth){
+        date = new Date()
+    
+        nowMonth = date.getMonth() + 1
         nowDay = date.getDate()
-        nowDay = neededDateDay - 1 - nowDay
-        // nowHours = 23
-        // nowMinutes = 59
-        // nowSeconds = 59
-    } 
-
-    if(nowDay.toString().length == 1){
-        nowDayStr = `0${nowDay}`
-    } else {
-        nowDayStr = `${nowDay}`
-    }
-    if(nowHours >= 14){
-        nowHoursStr = `0${23 - nowHours}`
-    } else {
-        nowHoursStr = `${23 - nowHours}`
-    }
-    if(nowMinutes >= 50){
-        nowMinutesStr = `0${59 - nowMinutes}`
-    } else {
-        nowMinutesStr = `${59 - nowMinutes}`
-    }
-    if(nowSeconds >= 50){
-        nowSecondsStr = `0${59 - nowSeconds}`
-    } else {
-        nowSecondsStr = `${59 - nowSeconds}`
-    }
-
-    timeTable.innerHTML = `
-       ${nowDayStr} : ${nowHoursStr} : ${nowMinutesStr} : ${nowSecondsStr}
-    `
-}, 500)
+        nowHours = date.getHours()
+        nowMinutes = date.getMinutes()
+        nowSeconds = date.getSeconds()
+        
+        if(nowMonth == 3){
+            nowDay = 31 + neededDateDay - 1 - nowDay
+        }
+        if(nowMonth == 2){
+            nowDay = 31 + 28 + neededDateDay - 1 - nowDay
+        }
+        if(nowMonth == neededDateMonth){
+            nowDay = date.getDate()
+            nowDay = neededDateDay - 1 - nowDay
+            // nowHours = 23
+            // nowMinutes = 59
+            // nowSeconds = 59
+        } 
+    
+        if(nowDay.toString().length == 1){
+            nowDayStr = `0${nowDay}`
+        } else {
+            nowDayStr = `${nowDay}`
+        }
+        if(nowHours >= 14){
+            nowHoursStr = `0${23 - nowHours}`
+        } else {
+            nowHoursStr = `${23 - nowHours}`
+        }
+        if(nowMinutes >= 50){
+            nowMinutesStr = `0${59 - nowMinutes}`
+        } else {
+            nowMinutesStr = `${59 - nowMinutes}`
+        }
+        if(nowSeconds >= 50){
+            nowSecondsStr = `0${59 - nowSeconds}`
+        } else {
+            nowSecondsStr = `${59 - nowSeconds}`
+        }
+    
+        timeTable.innerHTML = `
+           ${nowDayStr} : ${nowHoursStr} : ${nowMinutesStr} : ${nowSecondsStr}
+        `
+    }, 500)
+}
 
 
 const menuBtn = document.getElementById('menu_btn')
@@ -72,15 +75,39 @@ menuBtn.addEventListener("click", ()=>{
     } else {
         body.dataset.menuOpen = "true"
     }
-    console.log("a")
 });
 
 const p1Vid = document.getElementById('p1Vid');
 const p1vidClose = document.getElementById('p1vidClose');
 
-p1Vid.addEventListener('click', ()=>{
-    body.dataset.p1vidOpen = "true"
-})
-p1vidClose.addEventListener('click', ()=>{
-    body.dataset.p1vidOpen = "false"
-})
+if(p1Vid){
+    p1Vid.addEventListener('click', ()=>{
+        body.dataset.p1vidOpen = "true"
+    })
+    p1vidClose.addEventListener('click', ()=>{
+        body.dataset.p1vidOpen = "false"
+    })
+}
+
+const t_row_respbtns = document.getElementsByClassName('t_row_respbtn')
+const t_hid_rows = document.getElementsByClassName('t_hid_row')
+var canOpenHidrow = true
+
+
+for (let index = 0; index < t_row_respbtns.length; index++) {
+    const trbtn = t_row_respbtns[index];
+    const thidrow = t_hid_rows[index];
+    trbtn.addEventListener('click', ()=>{
+        console.log(thidrow.dataset.hidrowOpen)
+        if((thidrow.dataset.hidrowOpen == "false" || !(thidrow.dataset.hidrowOpen)) && canOpenHidrow){
+            thidrow.dataset.hidrowOpen = "true"
+            canOpenHidrow = false
+            setTimeout(()=>{canOpenHidrow = true}, 500)
+        }
+        if((thidrow.dataset.hidrowOpen == "true") && canOpenHidrow){
+            thidrow.dataset.hidrowOpen = "false"
+            canOpenHidrow = false
+            setTimeout(()=>{canOpenHidrow = true}, 500)
+        }
+    })
+}
